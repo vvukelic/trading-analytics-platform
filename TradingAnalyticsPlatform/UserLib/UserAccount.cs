61eb7ce979f8176c9589ad965f9a24c615769bc9
Vedran Vukelic <vedran.vukelic@gmail.com>
2017-03-26 00:06:50 +0100

    User lib added.

---
+++ TradingAnalyticsPlatform/UserLib/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLib
+{
+    public class UserAccount
+    {
+        public List<string> Tickers { get { return tickers; } }
+
+        private List<string> tickers = new List<string>();
+
+        public void AddTicker(string ticker)
+        {
+            tickers.Add(ticker);
+        }
+
+        public void AddTickers(string[] ticker_array)
+        {
+            tickers.AddRange(ticker_array);
+        }
+
+        public void RemoveTicker(string ticker)
+        {
+            tickers.Remove(ticker);
+        }
+
+        public void ClearTickers()
+        {
+            tickers.Clear();
+        }
+    }
+}
